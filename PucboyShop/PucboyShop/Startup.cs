c4b9c3127e9893d79cf458ae1023c61ada73b0cb
Moungjung Kim <moungjung@gmail.com>
2021-04-18 20:40:18 -0600

    Commit before trying migrations

--- PucboyShop/PucboyShop/Startup.cs
+++ PucboyShop/PucboyShop/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace PucboyShop
 {
@@ -24,6 +26,8 @@ namespace PucboyShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ PucboyShop/Shop.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+
+        public string Description { get; set; }
+    }
+
+}
